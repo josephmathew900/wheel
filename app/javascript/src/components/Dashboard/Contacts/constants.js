c6d7994aca226332950f35264e0163174af1d531
Joseph Mathew <josephmathew900@gmail.com>
2021-08-14 12:09:07 +0530

    Completed Contact list UI (#11)

---
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -0,0 +1,23 @@
+export const CONTACT_LIST = [
+  {
+    id: 1,
+    name: "Neeraj Singh",
+    email: "neeraj@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "(555)-390-102",
+    addToBaseCamp: false
+  },
+  {
+    id: 2,
+    name: "Vinay Chandran",
+    email: "vinay@bigbinary.com",
+    department: "Engineering",
+    contactNumber: "99210011001",
+    addToBaseCamp: false
+  }
+];
+
+export const SORT_OPTIONS = [
+  { label: "Name", value: "name" },
+  { label: "Department", value: "department" }
+];
